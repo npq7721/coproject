8d8db3d3de40e475ed9526fa81f2cbd7facfb8e2
tri <tri282@gmail.com>
2018-07-12 18:50:31 -0700

    fix collateral check

--- SlatechainCore/src/amount.h
+++ SlatechainCore/src/amount.h
@@ -16,6 +16,7 @@ typedef int64_t CAmount;
 static const CAmount COIN = 100000000;
 static const CAmount CENT = 1000000;
 static const CAmount MN_COLLATERAL = 2500 * COIN;
+static const CAmount MN_COLLATERAL_OUT = 2499.99 * COIN;
 
 /** Type-safe wrapper class to for fee rates
  * (how much to pay based on transaction size)
